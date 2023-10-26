d56916c270af57eaed95aa499c3d18dd81a5c8e8
Roberta Dutra <roberta.duttra@gmail.com>
2023-10-25 22:45:05 -0300

    created header and footer

--- src/app/app-rooting.module.ts
+++ src/app/app-rooting.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
